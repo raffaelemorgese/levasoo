814160636af281f65536f9090065c199218d7be1
biab <biab@biab_box>
2015-04-12 20:57:15 +0200

    Fancybox added.

--- view/box/footer.php
+++ view/box/footer.php
@@ -1,9 +1,16 @@
     <div class="footer">
       &reg; Copyright - RiccoPensa - Tutti i diritti riservati.
     </div>  
+    <!-- FANCYBOX -->
+      <script type="text/javascript">
+        $(document).ready(function() {
+          $(".fancybox").fancybox();
+        });
+      </script>
+    <!-- FANCYBOX -->
   </body>
 </html>
 <!-- MULTINESTEDLIST -->        
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> -->
 <script src="../view/box/lib/multinestedlist/multinestedlist.js"></script>
 <!-- MULTINESTEDLIST -->
--- view/box/header.php
+++ view/box/header.php
@@ -13,6 +13,9 @@
 	<meta name="Owner" content="riccopensa" /> 
 	<meta name="Author" content="riccopensa" />  
 	<meta name="Copyright" content="riccopensa" />
+        <!-- RICCOPENSA -->
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <!-- RICCOPENSA -->
         <!-- ALERTIFY -->
         <link rel="stylesheet" href="../view/box/lib/alertify/themes/alertify.core.css" media="all" />
         <link rel="stylesheet" href="../view/box/lib/alertify/themes/alertify.default.css" media="all" />
@@ -25,17 +28,17 @@
         <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" />
         <!-- MULTINESTEDLIST -->
         <!-- FANCYBOX -->
-        
+        <link rel="stylesheet" type="text/css" href="../view/box/lib/fancybox/source/jquery.fancybox.css?v=2.1.5" media="screen" />
+        <script type="text/javascript" src="../view/box/lib/fancybox/source/jquery.fancybox.pack.js"></script>
         <!-- FANCYBOX -->
-        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <!-- RICCOPENSA -->
         <script type="text/javascript" src="../view/box/js/riccopensa.js"></script>
         <link href='../view/box/css/riccopensa.css' TYPE="text/css" rel="stylesheet" />
-	<!-- We only want the thunbnails to display when javascript is disabled -->
+        <!-- RICCOPENSA -->
+        <!-- We only want the thunbnails to display when javascript is disabled -->
         <script type="text/javascript">
 		document.write('<style>.noscript { display: none; }</style>');
 	</script>
-    <!-- FANCYBOX -->
-    <!-- FANCYBOX -->
 
     </head>
 
